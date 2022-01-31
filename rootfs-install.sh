343d1c61893dfce28fa82a93646350cc98cea04c
Johnny <393116105@qq.com>
2022-01-31 14:55:31 +0800

    Update rootfs-install.sh

--- rootfs-install.sh
+++ rootfs-install.sh
@@ -1,6 +1,13 @@
 #!/bin/ash
 # https://github.com/sundaqiang/alpine-qinglong
 
+
+#初始化
+export LANG=UTF-8
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export SHELL=/bin/bash
+export PS1="\u@\h:\w \$ "
+
 #修改默认源
 sed -i '2,$d' /etc/apk/repositories \
 && echo http://mirrors.aliyun.com/alpine/v3.15/main/ >> /etc/apk/repositories \
@@ -10,41 +17,45 @@ sed -i '2,$d' /etc/apk/repositories \
 apk update -f \
 && apk upgrade \
 && apk --no-cache add -f bash \
-						 coreutils \
-						 moreutils \
-						 git \
-						 curl \
-						 wget \
-						 tzdata \
-						 perl \
-						 openssl \
-						 nginx \
-						 nodejs \
-						 npm \
-						 python3 \
-						 jq \
-						 openssh \
-						 py3-pip \
-						 python2 \
-						 g++ \
-						 make
+			coreutils \
+			moreutils \
+			git \
+			curl \
+			wget \
+			tzdata \
+			perl \
+			openssl \
+			nginx \
+			nodejs \
+			npm \
+			python3 \
+			jq \
+			openssh \
+			py3-pip \
+			python2 \
+			g++ \
+			make
 rm -rf /var/cache/apk/*
-
-#设置环境变量
-export LANG=UTF-8
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export SHELL=/bin/bash
-export PS1="\u@\h:\w \$ "
 
 #修改npm和pip镜像源
 pip config set global.index-url http://mirrors.aliyun.com/pypi/simple/ \
 && pip config set install.trusted-host mirrors.aliyun.com \
-&& npm config set registry http://registry.npm.taobao.org/
+&& npm config set registry https://registry.npmmirror.com
 
 #其他设置
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
 && echo "Asia/Shanghai" > /etc/timezone \
-&& touch ~/.bashrc
+&& touch ~/.bashrc \
+&& git config --global user.email "qinglong@@users.noreply.github.com" \
+&& git config --global user.name "qinglong" \
+&& npm install -g pnpm \
+&& pnpm install -g pm2 \
+&& pnpm install -g ts-node typescript tslib @types/node \
+&& cd / && pnpm install --prod \
+&& rm -rf /root/.npm \
+&& rm -rf /root/.pnpm-store \
+&& rm -rf /root/.cache \
+&& rm -f /package.json
 
 #安装qinglong
 QL_MAINTAINER="whyour"
@@ -53,18 +64,22 @@ QL_BRANCH=master
 QL_DIR=/ql
 rm -rf ${QL_DIR}
 git clone -b ${QL_BRANCH} ${QL_URL} ${QL_DIR} \
-&& git config --global user.email "qinglong@@users.noreply.github.com" \
-&& git config --global user.name "qinglong" \
 && cd ${QL_DIR} \
 && cp -f .env.example .env \
 && chmod 777 ${QL_DIR}/shell/*.sh \
 && chmod 777 ${QL_DIR}/docker/*.sh \
-&& npm install -g pnpm \
-&& pnpm install -g pm2 \
-&& pnpm install -g ts-node typescript tslib \
-&& rm -rf /root/.npm \
+&& cp -rf /node_modules ./ \
+&& rm -rf /node_modules \
 && pnpm install --prod \
+&& pnpm install ts-node typescript tslib date-fns axios ts-node typescript png-js crypto-js md5 ts-md5 tslib @types/node tough-cookie jsdom tunnel fs ws js-base64 got \
+&& pip3 install requests \
+&& pip3 install download \
+&& pip3 install canvas \
+&& pip3 install telethon \
+&& rm -rf /root/.npm \
 && rm -rf /root/.pnpm-store \
+&& rm -rf /root/.cache \
+&& apk --purge del python2 g++ make \
 && git clone -b ${QL_BRANCH} https://github.com/${QL_MAINTAINER}/qinglong-static.git /static \
 && cp -rf /static/* ${QL_DIR} \
 && rm -rf /static
@@ -81,7 +96,7 @@ echo '#!/bin/bash' > /start.sh \
 && echo 'rm -rf /var/cache/apk/*' >> /start.sh \
 && echo 'rm -rf /root/.npm' >> /start.sh \
 && echo 'rm -rf /root/.cache' >> /start.sh \
-&& echo 'rm -rf /usr/x86_64-alpine-linux-musl' >> /start.sh \
+&& echo 'rm -rf /root/.pnpm-store' >> /start.sh \
 && echo 'nohup ./docker/docker-entrypoint.sh &' >> /start.sh
 chmod +x /start.sh
 
